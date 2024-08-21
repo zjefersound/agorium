46c570a818c220ac5f66b7daacbf78c7923d4ce3
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-21 04:11:13 +0000

    Add Agorium favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,28 @@
 import { MdOutlineEdit } from "react-icons/md";
 import { Button } from "./components/ui/Button";
-import { SmartFieldExamples } from "./examples/SmartFieldExamples";
 
 function App() {
-  return <div className="flex flex-1 flex-col bg-agorium-900 text-agorium-50 overflow-auto">
-    <SmartFieldExamples />
-    <div className="p-4 space-y-4">
-      <Button color="primary"><MdOutlineEdit className="size-6 mr-2" /> Example</Button>
-      <Button color="secondary"><MdOutlineEdit className="size-6 mr-2" />Example</Button>
+  return (
+    <div className="flex flex-1 flex-col bg-agorium-900 text-agorium-50 overflow-auto">
+      {/* <SmartFieldExamples /> */}
+      <div className="p-4 space-y-4">
+        <Button color="primary">
+          <MdOutlineEdit className="size-6 mr-2" /> Example
+        </Button>
+        <Button color="secondary">
+          <MdOutlineEdit className="size-6 mr-2" />
+          Example
+        </Button>
+        <Button color="primary" size="sm">
+          <MdOutlineEdit className="size-5 mr-2" /> Edit
+        </Button>
+        <Button color="secondary" size="sm">
+          <MdOutlineEdit className="size-5 mr-2" />
+          Edit
+        </Button>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
