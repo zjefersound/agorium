765b84dd054d8941e14d561bcc8b068a8d937e9d
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-22 16:24:55 +0000

    Feat: Abstract home layout to Content component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,29 @@
+import { Content } from "../components/layout/Content";
 import { Card } from "../components/ui/Card";
 
 export function Home() {
   return (
-    <div
-      className={`
-        overflow-y-auto overflow-x-hidden
-        grid 
-        grid-cols-1
-        md:grid-cols-[var(--left-bar-width)_auto_var(--right-bar-width)]
-        px-[var(--page-padding-x)] justify-center
-      `}
-    >
-      <div className="hidden md:block space-y-6 py-6 sticky top-0 h-[var(--content-height)] overflow-auto">
+    <Content.Root>
+      <Content.Sidebar>
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
-      </div>
-      <div
-        className={`
-          py-6 px-[var(--main-content-padding-x)] space-y-6 
-          w-[var(--main-content-width)] 
-          max-w-[var(--main-content-max-width)] 
-        `}
-      >
+      </Content.Sidebar>
+      <Content.Main>
         <Card className="h-[300px]">Center</Card>
         <Card className="h-[300px]">Center</Card>
         <Card className="h-[300px]">Center</Card>
         <Card className="h-[300px]">Center</Card>
         <Card className="h-[300px]">Center</Card>
         <Card className="h-[300px]">Center</Card>
-      </div>
-      <div className="hidden md:block space-y-6 py-6 sticky top-0 h-[var(--content-height)] overflow-auto">
+      </Content.Main>
+      <Content.Sidebar>
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
-      </div>
-    </div>
+      </Content.Sidebar>
+    </Content.Root>
   );
 }
