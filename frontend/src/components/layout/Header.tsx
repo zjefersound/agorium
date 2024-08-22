42b4a82f509a5197e07d396496cf50212cd8ad56
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-22 16:25:04 +0000

    Change header tag

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -2,8 +2,8 @@ import { LogoHorizontal } from "../assets/LogoHorizontal";
 
 export function Header() {
   return (
-    <div className="h-[var(--header-height)] shrink-0 bg-agorium-800 sticky top-0 z-10 flex items-center px-[var(--page-padding-x)]">
+    <header className="h-[var(--header-height)] shrink-0 bg-agorium-800 sticky top-0 z-10 flex items-center px-[var(--page-padding-x)]">
       <LogoHorizontal />
-    </div>
+    </header>
   );
 }
