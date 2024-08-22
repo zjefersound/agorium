dafdf55d99b363451dd3bf4b75f7afe58d3f0980
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-22 14:26:20 +0000

    Fix: Responsive Layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,57 +2,40 @@ import { Card } from "../components/ui/Card";
 
 export function Home() {
   return (
-    <div className="px-[var(--page-padding-x)] grid grid-cols-[var(--left-bar-width)_auto_var(--right-bar-width)] justify-center overflow-auto">
-      <div className="space-y-6 py-6 sticky top-0 h-[calc(100dvh-var(--header-height))] overflow-auto">
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
+    <div
+      className={`
+        overflow-y-auto overflow-x-hidden
+        grid 
+        grid-cols-1
+        md:grid-cols-[var(--left-bar-width)_auto_var(--right-bar-width)]
+        px-[var(--page-padding-x)] justify-center
+      `}
+    >
+      <div className="hidden md:block space-y-6 py-6 sticky top-0 h-[var(--content-height)] overflow-auto">
+        <Card className="h-[200px]">asdsa</Card>
+        <Card className="h-[200px]">asdsa</Card>
+        <Card className="h-[200px]">asdsa</Card>
       </div>
-      <div className="w-[var(--main-content-width)] max-w-[var(--main-content-max-width)] p-6">
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
+      <div
+        className={`
+          py-6 px-[var(--main-content-padding-x)] space-y-6 
+          w-[var(--main-content-width)] 
+          max-w-[var(--main-content-max-width)] 
+        `}
+      >
+        <Card className="h-[300px]">Center</Card>
+        <Card className="h-[300px]">Center</Card>
+        <Card className="h-[300px]">Center</Card>
+        <Card className="h-[300px]">Center</Card>
+        <Card className="h-[300px]">Center</Card>
+        <Card className="h-[300px]">Center</Card>
       </div>
-      <div className="space-y-6 py-6 sticky top-0 h-[calc(100dvh-var(--header-height))] overflow-auto">
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
-        <Card>asdsa</Card>
-        <Card>Left 2</Card>
-        <Card>Left 2</Card>
+      <div className="hidden md:block space-y-6 py-6 sticky top-0 h-[var(--content-height)] overflow-auto">
+        <Card className="h-[200px]">asdsa</Card>
+        <Card className="h-[200px]">asdsa</Card>
+        <Card className="h-[200px]">asdsa</Card>
+        <Card className="h-[200px]">asdsa</Card>
+        <Card className="h-[200px]">asdsa</Card>
       </div>
     </div>
   );
