43968719961b3664a83a1016be01c0b2a10b4d33
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-22 20:41:38 +0000

    Feat: Introduce NavigationCard component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import { Content } from "../components/layout/Content";
 import { Card } from "../components/ui/Card";
+import { NavigationCard } from "../components/shared/NavigationCard";
 
 export function Home() {
   return (
     <Content.Root>
       <Content.Sidebar>
-        <Card className="h-[200px]">asdsa</Card>
+        <NavigationCard />
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
       </Content.Sidebar>
