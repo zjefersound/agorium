73723460ed63b3ce20d58ddc65a55f62cc82daa4
zjefersound <jeferson.2017317264@gmail.com>
2024-08-22 12:38:39 +0000

    Add css vars for complex UI layout

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import { LogoHorizontal } from "../assets/LogoHorizontal";
 
 export function Header() {
   return (
-    <div className="h-20 shrink-0 bg-agorium-800 sticky top-0 z-10 flex items-center px-8">
+    <div className="h-[var(--header-height)] shrink-0 bg-agorium-800 sticky top-0 z-10 flex items-center px-[var(--page-padding-x)]">
       <LogoHorizontal />
     </div>
   );
