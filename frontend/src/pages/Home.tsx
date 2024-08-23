caf28ef6134b7fbb0eb863da19b028145de2bf10
zjefersound <jeferson.2017317264@gmail.com>
2024-08-23 00:56:01 +0000

    Feat: Introduce SimpleUserCard component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Content } from "../components/layout/Content";
 import { Card } from "../components/ui/Card";
 import { NavigationCard } from "../components/shared/NavigationCard";
+import { SimpleUserCard } from "../components/shared/SimpleUserCard";
 
 export function Home() {
   return (
@@ -19,6 +20,14 @@ export function Home() {
         <Card className="h-[300px]">Center</Card>
       </Content.Main>
       <Content.Sidebar>
+        <SimpleUserCard
+          name="Joana Darc"
+          rankingPosition={12}
+          totalPosts={32}
+          totalUpvotes={642123}
+          username="@joanadarc"
+          url=""
+        />
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
         <Card className="h-[200px]">asdsa</Card>
