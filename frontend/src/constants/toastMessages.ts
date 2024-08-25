692170c8761c1193d9e7e18cd3ceda6dfbf9b078
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-25 14:11:59 +0000

    Improve Toast colors

---
+++ frontend/src/constants/toastMessages.ts
@@ -0,0 +1,11 @@
+export const TOAST_MESSAGES = {
+  Login: {
+    loginErrorTitle: "Error when trying to log in",
+  },
+  Signup: {},
+  common: {
+    unexpectedErrorTitle: "Unexpected error",
+    unexpectedErrorDescription:
+      "Verify your internet connection and try again later.",
+  },
+};
