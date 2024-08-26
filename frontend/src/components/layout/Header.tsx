267fec1ff68c933c6cb5307e4cf4149d6bd0da2e
zjefersound <jeferson.2017317264@gmail.com>
2024-08-26 02:36:44 +0000

    Add Logged in user on header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,9 +1,17 @@
+import { useAuth } from "../../hooks/useAuth";
 import { LogoHorizontal } from "../assets/LogoHorizontal";
+import { Avatar } from "../ui/Avatar";
 
 export function Header() {
+  const { user } = useAuth();
   return (
-    <header className="h-[var(--header-height)] shrink-0 bg-agorium-800 sticky top-0 z-10 flex items-center px-[var(--page-padding-x)]">
+    <header className="h-[var(--header-height)] shrink-0 bg-agorium-800 sticky top-0 z-10 flex items-center justify-between px-[var(--page-padding-x)]">
       <LogoHorizontal />
+
+      <div className="flex items-center space-x-4">
+        <Avatar name={user!.fullName} url={user!.avatar} />
+        <p className="font-serif text-amber-100">{user!.fullName}</p>
+      </div>
     </header>
   );
 }
