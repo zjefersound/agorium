096c518860f7aac0257cf17597c0d9e6a6ecc45e
gabrielmarafon <gabrielmarafon5@gmail.com>
2024-08-27 23:12:07 +0000

    fix: Key button group

--- frontend/src/components/ui/ButtonGroup.tsx
+++ frontend/src/components/ui/ButtonGroup.tsx
@@ -11,6 +11,7 @@ export function ButtonGroup({ value, onChange, options }: ButtonGroupProps) {
     <div className="h-10 rounded-md border-[1px] border-agorium-700 p-1 space-x-1 bg-agorium-800">
       {options.map((option) => (
         <button
+          key={option.value}
           className={clsx(
             "h-full rounded px-3 text-sm font-bold tracking-wider text-agorium-50",
             { "bg-agorium-600": value === option.value },
