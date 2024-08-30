f5e3b0d8c33c589981234268562debcffef6dd71
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-30 16:24:56 +0000

    Rename css file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./styles/index.css";
 import "@mdxeditor/editor/style.css";
 import "./styles/MDXEditor.css";
-import "./styles/CodeMirror.css";
+import "./styles/CodeMirrorDracula.css";
 import "./styles/Markdown.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
