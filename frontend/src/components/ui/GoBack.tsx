4c35ef5d0d9d8f938cab3e77ffa85505f6628e62
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2024-08-31 05:03:17 +0000

    Fix: adjustments on existing components

--- frontend/src/components/ui/GoBack.tsx
+++ frontend/src/components/ui/GoBack.tsx
@@ -1,19 +1,29 @@
+import clsx from "clsx";
 import { MdArrowBack } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 interface GoBackProps {
   to: string;
+  hideText?: boolean;
 }
-export function GoBack({ to }: GoBackProps) {
+export function GoBack({ to, hideText }: GoBackProps) {
   return (
     <Link
       to={to}
-      className="flex items-center hover:bg-agorium-800 w-min -mx-2 px-2 -my-1 py-1 rounded-full"
+      className={clsx(
+        "flex items-center hover:bg-agorium-800 w-min -mx-2 px-2 -my-1 py-1 rounded-full",
+        {
+          "-mx-1 px-1": hideText,
+          "-mx-2 px-2": !hideText,
+        },
+      )}
     >
-      <MdArrowBack className="size-6 text-amber-100 mr-3" />
-      <span className="text-agorium-400 text-sm tracking-wider whitespace-nowrap">
-        Go Back
-      </span>
+      <MdArrowBack className="size-6 text-amber-100" />
+      {!hideText && (
+        <span className="text-agorium-400 ml-3 text-sm tracking-wider whitespace-nowrap">
+          Go Back
+        </span>
+      )}
     </Link>
   );
 }
