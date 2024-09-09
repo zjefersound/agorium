aadee82ca06918f4f0a067f76b9edd251d3aae7a
zjefersound <jeferson.2017317264@gmail.com>
2024-09-09 16:36:18 +0000

    Abstract Text Editor styles

--- frontend/src/components/form/TextEditor.tsx
+++ frontend/src/components/form/TextEditor.tsx
@@ -21,62 +21,54 @@ import {
   CreateLink,
   InsertTable,
 } from "@mdxeditor/editor";
-import clsx from "clsx";
 import { memo } from "react";
 
-function TextEditor({ className, ...props }: MDXEditorProps) {
+function TextEditor({ ...props }: MDXEditorProps) {
   return (
-    <div
-      className={clsx(
-        "overflow-auto bg-agorium-800 ring-1 ring-agorium-700 rounded-md",
-        className,
-      )}
-    >
-      <MDXEditor
-        className="markdown dark-theme dark-editor h-full w-full cm-s-dracula"
-        plugins={[
-          headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
-          listsPlugin(),
-          linkPlugin(),
-          quotePlugin(),
-          linkDialogPlugin(),
-          imagePlugin(),
-          tablePlugin(),
-          codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
-          codeMirrorPlugin({
-            codeBlockLanguages: {
-              js: "JavaScript",
-              ts: "Typescript",
-              css: "CSS",
-              html: "HTML",
-              php: "PHP",
-              python: "Python",
-              java: "Java",
-              c: "C",
-              csharp: "C#",
-              bash: "Bash",
-              shell: "Shell",
-            },
-          }),
-          toolbarPlugin({
-            toolbarContents: () => (
-              <>
-                <UndoRedo />
-                <BlockTypeSelect />
-                <BoldItalicUnderlineToggles />
-                <ListsToggle />
-                <CodeToggle />
-                <InsertCodeBlock />
-                <CreateLink />
-                <InsertTable />
-              </>
-            ),
-          }),
-          markdownShortcutPlugin(),
-        ]}
-        {...props}
-      />
-    </div>
+    <MDXEditor
+      className="markdown dark-theme dark-editor h-full w-full cm-s-dracula"
+      plugins={[
+        headingsPlugin({ allowedHeadingLevels: [1, 2, 3] }),
+        listsPlugin(),
+        linkPlugin(),
+        quotePlugin(),
+        linkDialogPlugin(),
+        imagePlugin(),
+        tablePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "js" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: {
+            js: "JavaScript",
+            ts: "Typescript",
+            css: "CSS",
+            html: "HTML",
+            php: "PHP",
+            python: "Python",
+            java: "Java",
+            c: "C",
+            csharp: "C#",
+            bash: "Bash",
+            shell: "Shell",
+          },
+        }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              <UndoRedo />
+              <BlockTypeSelect />
+              <BoldItalicUnderlineToggles />
+              <ListsToggle />
+              <CodeToggle />
+              <InsertCodeBlock />
+              <CreateLink />
+              <InsertTable />
+            </>
+          ),
+        }),
+        markdownShortcutPlugin(),
+      ]}
+      {...props}
+    />
   );
 }
 
