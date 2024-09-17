90a238c06265404d6b3692950ac1cdb6250a2e74
Jeferson Souza <jeferson.2017317264@gmail.com>
2024-09-17 16:16:27 +0000

    Create comment service

--- frontend/src/services/commentService.ts
+++ frontend/src/services/commentService.ts
@@ -1,18 +1,41 @@
+import { Comment } from "../models/Comment";
+import { IPaginatedResponse } from "../models/IPaginatedResponse";
+import { ISearchableOptions } from "../models/ISearchableOptions";
+import { api } from "./api";
+
 export type CommentPayload = {
   content: string;
   parentCommentId?: number;
 };
 
 function create(postId: number, payload: CommentPayload) {
-  console.log([`/post/${postId}/comment`, payload]);
+  return api.post(`/post/${postId}/comment`, payload);
+}
 
-  return Promise.resolve();
+function getById(postId: number, commentId: number) {
+  return api.get<Comment>(`/post/${postId}/comment/${commentId}`);
+}
+
+function update(postId: number, commentId: number, payload: CommentPayload) {
+  return api.put(`/post/${postId}/comment/${commentId}`, payload);
+}
+
+function deleteComment(postId: number, commentId: number) {
+  return api.delete(`/post/${postId}/comment/${commentId}`);
+}
+
+function getAll(postId: number, options?: ISearchableOptions) {
+  return api.get<IPaginatedResponse<Comment>>(`/post/${postId}/comments`, {
+    params: {
+      ...options,
+    },
+  });
 }
 
 export const commentService = {
   create,
-  // delete: deleteComment,
-  // getAll,
-  // getById,
-  // update,
+  delete: deleteComment,
+  getAll,
+  getById,
+  update,
 };
