9732424bc98db801e53b41b59c22316442103ec7
zjefersound <jeferson.2017317264@gmail.com>
2024-09-17 22:22:26 +0000

    Create GlobalSidebar component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,39 +1,16 @@
 import { Content } from "../components/layout/Content";
-import { NavigationCard } from "../components/shared/NavigationCard";
 import { SimpleUserCard } from "../components/shared/SimpleUserCard";
-import { PopularItemCard } from "../components/shared/PopularItemCard";
 import { TrendingPosts } from "../components/shared/TrendingPosts";
 import { mockedPosts, rankingCardItems } from "../examples/mocks/mocks";
 import { RankingCard } from "../components/shared/RankingCard";
 import { HomeContent } from "../containers/HomeContent";
+import { GlobalSidebar } from "../components/shared/GlobalSidebar";
 
 export function Home() {
   return (
     <Content.Root>
       <Content.Sidebar>
-        <NavigationCard />
-        <PopularItemCard
-          title="Popular tags"
-          path="/tags"
-          items={[
-            { id: 1, label: "#biology", totalPosts: 53 },
-            { id: 7, label: "#math", totalPosts: 43 },
-            { id: 8, label: "#science", totalPosts: 41 },
-            { id: 9, label: "#englsih", totalPosts: 31 },
-            { id: 10, label: "#history", totalPosts: 12 },
-          ]}
-        />
-        <PopularItemCard
-          title="Popular categories"
-          path="/categories"
-          items={[
-            { id: 2, label: "Issue", totalPosts: 286 },
-            { id: 3, label: "Discussion", totalPosts: 233 },
-            { id: 4, label: "Feedback", totalPosts: 211 },
-            { id: 5, label: "Debate", totalPosts: 173 },
-            { id: 6, label: "Tutorials", totalPosts: 163 },
-          ]}
-        />
+        <GlobalSidebar />
       </Content.Sidebar>
       <Content.Main>
         <HomeContent />
---
+++ frontend/src/pages/ProfileEdit.tsx
@@ -0,0 +1,3 @@
+export function ProfileEdit() {
+  return <div>ProfileEdit</div>;
+}
