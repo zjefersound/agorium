2c68c07f69b0a1a049bd534681d3932bc893284c
zjefersound <jeferson.2017317264@gmail.com>
2024-09-25 00:15:03 +0000

    Remove tabs from profile

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -2,20 +2,14 @@ import { Content } from "../components/layout/Content";
 import { TrendingPosts } from "../components/shared/TrendingPosts";
 import { ProfileCard } from "../containers/profile/ProfileCard";
 import { GlobalSidebar } from "../components/shared/GlobalSidebar";
-import { Tabs } from "../components/ui/Tabs";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { PostList } from "../components/shared/PostList";
 import { useAuth } from "../hooks/useAuth";
 import { ConnectedUserCard } from "../components/shared/ConnectedUserCard";
 import { ConnectedRankingCard } from "../components/shared/ConnectedRankingCard";
 
 export function Profile() {
-  const [selectedTab, setSelectedTab] = useState("post");
   const { user } = useAuth();
-
-  const handleTabChange = (newValue: string) => {
-    setSelectedTab(newValue);
-  };
 
   const postsFilter = useMemo(() => {
     return { userId: String(user!.id) };
@@ -28,16 +22,6 @@ export function Profile() {
       </Content.Sidebar>
       <Content.Main>
         <ProfileCard />
-        <Tabs
-          value={selectedTab}
-          onChange={handleTabChange}
-          options={[
-            { label: "Posts", value: "posts" },
-            { label: "Comments", value: "comments" },
-            { label: "Upvotes", value: "upvotes" },
-          ]}
-          placement="left"
-        />
         <PostList filter={postsFilter} />
       </Content.Main>
       <Content.Sidebar>
