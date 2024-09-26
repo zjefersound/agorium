28ceb6a8372da55ddd1d1110044ede810a5b6b9f
gabrielmarafon <gabrielmarafon5@gmail.com>
2024-09-26 00:28:11 +0000

    feat: Criação de not found e splash screen

--- frontend/src/components/layout/SplashScreen.tsx
+++ frontend/src/components/layout/SplashScreen.tsx
@@ -1,5 +1,10 @@
+import Lottie from "lottie-react";
+import emptyAnimation from "../../assets/splash-screen.json";
+
 export function SplashScreen() {
   return (
-    <div>SplashScreen</div>
-  )
+    <div className="m-auto size-[200px]">
+      <Lottie animationData={emptyAnimation} loop={true} />
+    </div>
+  );
 }
