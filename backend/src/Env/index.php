e9791d23685d8d62269363ebaeb3d00d33babc7c
Jeferson Souza <jeferson.2017317264@gmail.com>
2024-10-01 00:57:12 +0000

    Add vars to queue helper

--- backend/src/Env/index.php
+++ backend/src/Env/index.php
@@ -1,12 +1,12 @@
 <?php
 
 $env = file_get_contents(__DIR__ . "/../../.env");
-$lines = explode("\n",$env);
+$lines = explode("\n", $env);
 
-foreach($lines as $line){
-  preg_match("/([^#]+)\=(.*)/",$line,$matches);
-  
-  if(isset($matches[2])){
+foreach ($lines as $line) {
+  preg_match("/([^#]+)\=(.*)/", $line, $matches);
+
+  if (isset($matches[2])) {
     putenv(trim($line));
   }
-} 
+}
