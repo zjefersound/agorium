af896930821de991839692263f76920355393e05
Jeferson Souza <jeferson.2017317264@gmail.com>
2024-10-01 12:08:49 -0300

    Deploy services to vercel + aws

---
+++ backend/src/Middleware/CorsMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class CorsMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        // Process request and get response
+        $response = $handler->handle($request);
+
+        // Add CORS headers
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    }
+}
